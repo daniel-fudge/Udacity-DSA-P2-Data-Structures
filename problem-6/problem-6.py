24a91aac09266078353763ed8163198f7eb145c9
Daniel Fudge <daniel.fudge1977@gmail.com>
2022-05-17 11:37:58 -0400

    Test 6 operational and given test pass, need to add more user tests.

--- problem-6/problem-6.py
+++ problem-6/problem-6.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
-"""Problem 5 of the Data Structures Project.
-
+"""Problem 6 of the Data Structures Project.
 
 Notes:
+    1. This code could have been simplified by using the builtin Python set Type and the union and intersection
+        operations. They weren't used to illustrate the basic functionality.
 
 Assumptions:
-
+    1. Duplicates in the input lists are ignored, therefore no duplicates in the union and intersection output.
+    2. Order of the linked lists does not need to be maintained.
 """
 
 from __future__ import annotations
@@ -15,53 +17,140 @@ import hashlib
 from time import time
 
 
-class Block(object):
-
-    def __init__(self, timestamp: str, data: str, previous_hash: str):
-        self.timestamp = timestamp
-        self.data = data
-        self.previous_hash = previous_hash
-        self.hash = self.calc_hash()
+class Node:
+    def __init__(self, value):
+        self.value = value
         self.next = None
 
-    def calc_hash(self):
-        sha = hashlib.sha256()
-        sha.update(self.data.encode('utf-8'))
-        return sha.hexdigest()
+    def __repr__(self):
+        return str(self.value)
 
 
-class BlockChain(object):
+class LinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
         self.size = 0
 
-    def append(self, data: str):
-        timestamp = datetime.utcnow().strftime('%d-%m-%Y %H:%M:%S')
+    def __str__(self):
+        cur_head = self.head
+        out_string = ""
+        while cur_head:
+            out_string += str(cur_head.value) + " -> "
+            cur_head = cur_head.next
+        return out_string
+
+    def append(self, value):
+        new_node = Node(value)
         if self.head is None:
-            self.head = Block(timestamp=timestamp, data=data, previous_hash="")
+            self.head = new_node
             self.tail = self.head
         else:
-            new_block = Block(timestamp=timestamp, data=data, previous_hash=self.tail.hash)
-            self.tail.next = new_block
-            self.tail = new_block
+            self.tail.next = new_node
+            self.tail = new_node
         self.size += 1
-
-    def get_head(self):
-        return self.head
 
     def __len__(self):
         return self.size
 
-    def __repr__(self):
-        if self.size == 0:
-            return "Empty BlockChain"
-        message = "Timestamp | Data | Hash | Previous Hash (Starting at the head)\n"
+    def get_unique_values(self) -> list:
+        """Return the unique values of the linked list.
+
+        Returns:
+            list: The unique values in the linked list.
+        """
+        values = []
         node = self.head
         while node:
-            message += " | ".join([node.timestamp, node.data, node.hash, node.previous_hash]) + "\n"
+            if node.value not in values:
+                values.append(node.value)
             node = node.next
-        return message
+        return values
+
+
+def intersection(list1: LinkedList, list2: LinkedList) -> LinkedList:
+    """Generates a new Linked List that is the intersection of the two input Linked lists.
+
+    Note: Duplicates in the input lists are ignores and order is not maintained so:
+        [1, 1, 2, 3, 3] intersection [2, 2, 2, 3] = [2, 3] or [3, 2]
+
+    Args:
+        list1 (LinkedList): The first linked list to process.
+        list2 (LinkedList): The second linked list to process.
+
+    Returns:
+        LinkedList: The union of the two input linked lists.
+    """
+    unique_values1 = list1.get_unique_values()
+    unique_values2 = list2.get_unique_values()
+    new_list = LinkedList()
+    for v in [v for v in unique_values1 if v in unique_values2]:
+        new_list.append(v)
+
+    return new_list
+
+
+def union(list1: LinkedList, list2: LinkedList) -> LinkedList:
+    """Generates a new Linked List that is the union of the two input Linked lists.
+
+    Note: Duplicates in the input lists are ignores and order is not maintained so:
+        [1, 1, 1] union [2, 2, 2] = [1, 2] or [2, 1]
+
+    Args:
+        list1 (LinkedList): The first linked list to process.
+        list2 (LinkedList): The second linked list to process.
+
+    Returns:
+        LinkedList: The union of the two input linked lists.
+    """
+    new_list = LinkedList()
+    unique_values1 = list1.get_unique_values()
+    for v in unique_values1:
+        new_list.append(v)
+    for v in [v for v in list2.get_unique_values() if v not in unique_values1]:
+        new_list.append(v)
+
+    return new_list
+
+
+def given_tests():
+
+    test = 0
+    for element_1, element_2 in [([3, 2, 4, 35, 6, 65, 6, 4, 3, 21], [6, 32, 4, 9, 6, 1, 11, 21, 1]),
+                                 ([3, 2, 4, 35, 6, 65, 6, 4, 3, 23], [1, 7, 8, 9, 11, 21, 1])]:
+        test += 1
+        print(f"\nGiven test {test}")
+
+        linked_list_1 = LinkedList()
+        for i in element_1:
+            linked_list_1.append(i)
+
+        linked_list_2 = LinkedList()
+        for i in element_2:
+            linked_list_2.append(i)
+
+        print(f"List 1: {linked_list_1}")
+        print(f"List 2: {linked_list_2}")
+
+        # Check the union function
+        actual = union(linked_list_1, linked_list_2)
+        expected = set(element_1).union(element_2)
+        print(f"\nUnion: {actual}")
+        errors = expected.symmetric_difference(actual.get_unique_values())
+        if len(errors) == 0:
+            print(f"Test {test} union correct, expected and actual values are the same.\n")
+        else:
+            print(f"Error test {test}: errors = {errors}.\n")
+
+        # Check the intersection function
+        actual = intersection(linked_list_1, linked_list_2)
+        expected = set(element_1).intersection(element_2)
+        print(f"Intersection: {actual}")
+        errors = expected.symmetric_difference(actual.get_unique_values())
+        if len(errors) == 0:
+            print(f"Test {test} intersection, expected and actual values are the same.\n")
+        else:
+            print(f"Error test {test}: errors = {errors}.\n")
 
 
 # noinspection PyBroadException
@@ -74,94 +163,6 @@ def user_tests():
     # Check the BlockChain print functionality
     print("\nUser test set 1 - print functionality.")
     test = 1
-    chain = BlockChain()
-    for b in range(5):
-        chain.append(data=f"block {b+1}")
-    try:
-        print(chain)
-    except:
-        print(f"Error test {test}: Wasn't expecting an exception.")
-        n_errors += 1
-    else:
-        print(f"Test {test} passed, well really have to look hash and previous hash values to verify a pass.")
-
-    # Check the previous hash values
-    print("\nUser test set 2 - Check previous hash values.")
-    test = 0
-    node = chain.get_head()
-    for _ in range(4):
-        test += 1
-        next_node = node.next
-        if next_node.previous_hash != node.hash:
-            print(f"Error test {test}: Hash values don't match.")
-            n_errors += 1
-        else:
-            print(f"Test {test} passed.")
-        node = next_node
-
-    # Check repeated data
-    print("\nUser test set 3 - Repeated data.")
-    test = 0
-    chain = BlockChain()
-    for b in range(5):
-        chain.append(data=f"Repeated Data")
-    node = chain.get_head()
-    for _ in range(4):
-        test += 1
-        next_node = node.next
-        if next_node.hash != node.hash:
-            print(f"Error test {test}: Hash values don't match.")
-            n_errors += 1
-        else:
-            print(f"Test {test} passed.")
-        node = next_node
-
-    # Test invalid arguments
-    print("\nUser test set 4 - Invalid arguments.")
-    test = 0
-    chain = BlockChain()
-    for arg in [1, [], {}, None]:
-        test += 1
-        try:
-            # noinspection PyTypeChecker
-            chain.append(arg)
-        except AttributeError:
-            print(f"Test {test} passed.")
-        else:
-            print(f"Error test {test}: expected an AttributeError exception.")
-            n_errors += 1
-
-    # Check the hash values are unique
-    print("\nUser test set 5 - Hash uniqueness for a million values, also test large chains and speed.")
-    test = 1
-    print("Building the million node BlockChain")
-    start_time = time()
-    chain = BlockChain()
-    n = 10**6
-    for b in range(n):
-        chain.append(data=f"block {b+1}")
-    print(f"\tBuilding took {time()-start_time:.2f} seconds.")
-
-    print("Checking all hash values in the chain for uniqueness.")
-    start_time = time()
-    node = chain.get_head()
-    values = [node.hash]
-    node = node.next
-    while node:
-        new_hash = node.hash
-        if new_hash in values:
-            print(f"Error test {test}: Hash values repeated.")
-            n_errors += 1
-            break
-        node = node.next
-    print(f"\tChecking took {time()-start_time:.2f} seconds.")
-
-    test += 1
-    if len(chain) == n:
-        print("Length of chain also verified")
-    else:
-        print(f"Error test {test}: expected a length of {n} but got {len(chain)}.")
-        n_errors += 1
 
     print("\n*******************")
     if n_errors > 0:
@@ -172,4 +173,5 @@ def user_tests():
 
 # **********************************************************
 if __name__ == '__main__':
+    given_tests()
     user_tests()
